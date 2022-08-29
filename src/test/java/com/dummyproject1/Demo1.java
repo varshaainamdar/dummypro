a5c93b9f0b9e32ddff1ceca4f93578b207035198
LENOVO <LENOVO@LENOVO-PC>
2022-08-29 10:22:01 +0530

    created

---
+++ src/test/java/com/dummyproject1/Demo1.java
@@ -0,0 +1,11 @@
+package com.dummyproject1;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("create a test");
+
+	}
+
+}
---
+++ src/test/java/com/dummyproject1/Demo2.java
@@ -0,0 +1,11 @@
+package com.dummyproject1;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("create test2");
+
+	}
+
+}
